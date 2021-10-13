a01401c5bf02a191613819e4843b19a48f9b8d58
Sipos Miklos <commit@siposm.hu>
2021-10-13 12:40:16 +0200

    test initialized

--- BlogSystem/BlogSystem.Tests/Tests.cs
+++ BlogSystem/BlogSystem.Tests/Tests.cs
@@ -1,8 +1,20 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace BlogSystem.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class BlogLogicTests
     {
+        // NuGets below needed to be installed:
+        //      nunit 3
+        //      nunit 3 testadapter
+        //      microsoft testing sdk
+
+        [Test]
+        public void Test1()
+        {
+            Assert.That(true, Is.True);
+        }
     }
 }
